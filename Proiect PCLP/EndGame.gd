395ba20f5cafd6492a00d001f4d717896e18ce98
ObrejanAlin <115227823+ObrejanAlin@users.noreply.github.com>
2023-01-12 23:23:00 +0200

    background main menu + endgame menu finalizat

--- Proiect PCLP/EndGame.gd
+++ Proiect PCLP/EndGame.gd
@@ -9,17 +9,15 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-	$VBoxContainer/StartButton.grab_focus()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_StartButton_pressed():
+	
+func _on_RestartButton_pressed():
 	get_tree().change_scene("res://dungeon.tscn")
 
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
